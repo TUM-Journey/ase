59d70fa32abe636e18b55d2b7bf24968f7408292
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-12-09 18:41:45 +0100

    Fix ant matcher of authenticated resources

--- backend/src/main/java/de/tum/ase/kleo/application/auth/ResourceServerConfig.java
+++ backend/src/main/java/de/tum/ase/kleo/application/auth/ResourceServerConfig.java
@@ -27,6 +27,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                     .and()
                 .authorizeRequests()
                 .antMatchers("/actuator/**", "/api-docs/**").permitAll()
-                .antMatchers("/api/**" ).authenticated();
+                .antMatchers("/**").authenticated();
     }
 }
