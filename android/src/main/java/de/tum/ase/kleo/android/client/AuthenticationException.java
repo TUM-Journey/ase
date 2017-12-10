62f4abbce7659f20513145a95672a005669419ad
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-12-10 09:29:44 +0100

    Force BackendClient to validate credentials on construct

---
+++ android/src/main/java/de/tum/ase/kleo/android/client/AuthenticationException.java
@@ -0,0 +1,12 @@
+package de.tum.ase.kleo.android.client;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- android/src/main/java/de/tum/ase/kleo/android/client/BackendClient.java
+++ android/src/main/java/de/tum/ase/kleo/android/client/BackendClient.java
@@ -1,28 +1,36 @@
 package de.tum.ase.kleo.android.client;
 
+import java.io.IOException;
+
 import de.tum.ase.kleo.android.client.invoker.ApiClient;
 import de.tum.ase.kleo.android.client.invoker.auth.OAuth;
 import de.tum.ase.kleo.android.client.invoker.auth.OAuthFlow;
-
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class BackendClient extends ApiClient {
 
     private static final String OAUTH_TOKEN_ENDPOINT = "oauth/token";
 
-    public BackendClient(String basePath, String clientId, String secret, String username, String password) {
+    public BackendClient(String basePath, String clientId, String secret,
+                         String username, String password) {
         super();
         super.setAdapterBuilder(getAdapterBuilder().baseUrl(basePath));
 
         final OAuth oAuth = new OAuth(OAuthFlow.password, "", basePath + OAUTH_TOKEN_ENDPOINT, null);
-        super.addAuthorization("OAuth2Password", oAuth);
+        super.addAuthorization(oAuth.getClass().getName(), oAuth);
 
         super.setCredentials(username,  password);
         super.getTokenEndPoint().setClientId(clientId).setUsername(username).setPassword(password);
         if (secret != null) super.getTokenEndPoint().setClientSecret(secret);
+
+        try {
+            oAuth.updateAccessToken(null);
+        } catch (IOException e) {
+            throw new AuthenticationException("Failed to log in with given credentials", e);
+        }
     }
 
-    public BackendClient(String basePath, String clientId, String username, String password) {
+    public BackendClient(String basePath, String clientId,
+                         String username, String password) {
         this(basePath, clientId, null, username, password);
     }
 }
