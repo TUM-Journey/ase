ba16f7eb4296b76aab3b08dc5ee6e17f14ad0188
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2017-12-10 11:58:28 +0100

    Add user login view stub

--- android/src/main/java/de/tum/ase/kleo/android/LoginActivity.java
+++ android/src/main/java/de/tum/ase/kleo/android/LoginActivity.java
@@ -1,7 +1,11 @@
 package de.tum.ase.kleo.android;
 
 import android.app.Activity;
+import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +13,16 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.loginSubmit).setOnClickListener(new LoginSubmitListener());
+    }
+
+    public class LoginSubmitListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String email = ((EditText) findViewById(R.id.loginEmail)).getText().toString();
+            String password = ((EditText) findViewById(R.id.loginPassword)).getText().toString();
+        }
     }
 }
