86787fc01d4583d99629dbcc426e7aa900f40112
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-01-23 20:06:52 +0100

    Add .solidity slc output to .gitignore

--- attendance_tracker_gen.sh
+++ attendance_tracker_gen.sh
@@ -7,5 +7,3 @@ solc attendance_tracker.sol --bin --abi --optimize -o ./.solidity/
 web3j solidity generate .solidity/AttendanceTracker.bin .solidity/AttendanceTracker.abi -p de.tum.ase.kleo.ethereum -o .solidity/
 cp -r .solidity/de ./backend/src/main/java
 cp -r .solidity/de ./android/src/main/java
-
-rm -rf .solidity/ 2> /dev/null
