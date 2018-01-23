d9bf2a54f6f41da62a0629d4791ea2a6cd80bd17
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2018-01-23 18:47:08 +0100

    Simplify AttendanceTracker Smart Contract

--- attendance_tracker.sol
+++ attendance_tracker.sol
@@ -1,36 +1,26 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.19;
 
 contract AttendanceTracker {
-    
-    struct Attendance {
-        string studentId;
-        uint attendedAt;
+
+    // Maps sessionId => studentId[]
+    mapping(string => string[]) private attendances;
+
+    function recordAttendance(string sessionId, string studentId) public {
+        attendances[sessionId].push(studentId);
     }
-    
-    mapping(string => Attendance[]) sessionsAttendances;
-    
-    function recordAttendance(string sessionId, string studentId, uint attendedAt) public {
-        sessionsAttendances[sessionId].push(Attendance(studentId, attendedAt));
-    }
-    
-    function hasAttended(string sessionId, string studentId, uint timestamp, uint allowedDelta) public constant returns(bool) {
-        Attendance[] memory sessionAttendances = sessionsAttendances[sessionId];
-        
+
+    function hasAttented(string sessionId, string studentId) public constant returns(bool) {
+        string[] memory sessionAttendances = attendances[sessionId];
+
         if (sessionAttendances.length == 0) {
             return false;
         }
-        
+
         for (uint i=0; i<sessionAttendances.length; i++) {
-            if (keccak256(sessionAttendances[i].studentId) != keccak256(studentId))
-                continue;
-            
-            int timestampsDelta = int(timestamp - sessionAttendances[i].attendedAt);
-            if (abs(timestampsDelta) <= allowedDelta)
+            if (keccak256(sessionAttendances[i]) == keccak256(studentId))
                 return true;
         }
-        
+
         return false;
     }
-    
-    function abs(int input) pure private returns (uint){ return uint( (input < 0) ? -input : input ); }
 }
